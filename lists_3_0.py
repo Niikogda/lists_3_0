a95806fbccec119351da966c9f6b5241f41733b2
Niiikogda <niikogda@gmail.com>
2023-04-02 11:02:08 +0300

    update

--- lists_3_0.py
+++ lists_3_0.py
@@ -1,1 +1,11 @@
 
+try:
+  user_list=input("enter numbers for list with comma: ")
+  list=[int(nums) for nums in user_list.split(",")]
+  #list.append(user_list)
+  list.reverse()
+  print(list)
+except:
+  print("input numbers with comma")
+finally:
+        print ('done')
