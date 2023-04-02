136530e78be284690b3efdb62d745e823a4096e5
Niiikogda <niikogda@gmail.com>
2023-04-02 10:42:54 +0300

    done_0

--- lists_3_0.py
+++ lists_3_0.py
@@ -1,1 +1,16 @@
 
+user_list=input("enter your list pls: ")
+list=[]
+list.append(user_list) 
+nums=0
+lett=0
+for i in list:
+    if i.isalpha():
+        lett+=1
+    elif i.isnumeric():
+        nums+=1
+    else:
+        lett+=0
+        nums+=0
+print("amount of letters= ", lett, "amount of numbers= ", nums)
+print(list)
