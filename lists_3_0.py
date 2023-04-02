72567d258269615b616eab4338d7e04e2bbdd4b7
Niiikogda <niikogda@gmail.com>
2023-04-02 11:33:54 +0300

    done_4

--- lists_3_0.py
+++ lists_3_0.py
@@ -1,1 +1,10 @@
+user_string=input('enter string: ')
+user_word=input('enter word you wanna count: ')
+word=user_string.split()
+amount=0
+for w in word:
+    if w==user_word:
+        amount+=1
+print('amount of', user_word, 'is:', amount)
+
 
